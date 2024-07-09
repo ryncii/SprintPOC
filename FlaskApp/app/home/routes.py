880f73db8c3d2e6341afa82c28f5c60cb3d202fb
Ryan <ryanraphaelchia@outlook.com>
2024-07-09 19:59:26 +0800

    temp base

--- FlaskApp/app/home/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, request
-from app.home import bp
-
-@bp.route('/')
-def load():
-    #squeeze = squeezeThisTextIn()
-    #fig = buildSampleGraphImage()
-    #graphJSON = json.dumps(fig, cls=pUtils.PlotlyJSONEncoder)
-    return render_template('home.html', INPUT = request.form['username'])
--- FlaskApp/app/login/routes.py
+++ FlaskApp/app/login/routes.py
@@ -3,12 +3,4 @@ from app.login import bp
 
 @bp.route('/')
 def load():
-    print('here')
     return render_template('login.html')
-
-'''
-@bp.route('/handle_authentication', method=['POST'])
-def authenticate():
-    print('here2')
-    return render_template('login.html')
-'''

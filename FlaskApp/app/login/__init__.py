1e3c44b79f5ce5de869301d296f4c8bdbef69061
Ryan <ryanraphaelchia@outlook.com>
2024-07-09 22:11:15 +0800

    post works

--- FlaskApp/app/login/__init__.py
+++ FlaskApp/app/login/__init__.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request as flaskReq
 
 bp = Blueprint('login', __name__)
 
 @bp.route('/')
 def load():
+    return render_template('login.html')
+
+@bp.route('/authenticate', methods=['POST'])
+def authenticate():
+    print(flaskReq.form['username'])
     return render_template('login.html')

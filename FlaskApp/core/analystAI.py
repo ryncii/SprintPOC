57505de7f77ff75dcaf66430b10fcb7d83d3021b
Ryan <ryanraphaelchia@outlook.com>
2024-09-09 10:49:42 +0800

    Commit - Inconsiquential

--- FlaskApp/core/analystAI.py
+++ FlaskApp/core/analystAI.py
@@ -1,6 +1,15 @@
 import nltk, pandas as pd
 import sklearn.naive_bayes as nb
 import sklearn.preprocessing as skprocess
+from sklearn import svm
+
+from copy import copy
+import pandas as pd, os
+
+
+POS_ENCODER = dict(CC=1,CD=2,DT=3,EX=4,FW=5,IN=6,JJ=7,JJR=8,JJS=9,LS=10,MD=11,NN=12,NNS=13,NNP=14,NNPS=15,PDT=16,POS=17,PRP=18,RB=19,RBR=20,RBS=21,RP=22,TO=23,UH=24,VB=25,VBD=26,VBG=27,VBN=28,VBP=29,VBZ=30,WDT=31,WP=32,WRB=33)
+CONTEXTRESULT_ENCODER = dict(General = 0, Specific = 1)
+
 
 class AnalystAI:
 
@@ -11,7 +20,6 @@ class AnalystAI:
         self.inMemoryData = []
 
 
-
         # NLTK
         nltk.download('stopwords')
         nltk.download('averaged_perceptron_tagger')
@@ -19,6 +27,9 @@ class AnalystAI:
         self.stopwords = set(nltk.corpus.stopwords.words("english"))
         self.stemmer = nltk.stem.PorterStemmer()
         self.lemmatizer = nltk.stem.WordNetLemmatizer()
+
+
+        
 
 
 
@@ -62,15 +73,29 @@ class AnalystAI:
         trainCols = [col for col in list(train) if 'i_' in col ]
         trainTarget = 'Output'
 
-        train_i = [encoder.fit_transform(se) for ind, se in train[trainCols].iterrows()]
+        encodedTrain = copy(train[trainCols])
+
+        for col in trainCols:
+            encodedTrain[col] = encoder.fit_transform(encodedTrain[col])
+
+        train_i = [se for ind, se in encodedTrain.iterrows()]
         train_o = encoder.fit_transform(train[trainTarget])
-        
+
+        i = 0
+        self.outputDecoder = {} 
+        while i < len(train_o):
+            self.outputDecoder[train_o[i]] = train[trainTarget][i]
+            i += 1
+
+        print(self.outputDecoder )
         self.model = nbModel.fit(train_i, train_o)
+        
         # to Reverse
         # le.inverse_transform([0,1,2])
 
     def processQuestion(self, question: str):
         tokenized = nltk.word_tokenize(question.lower())
+        print(tokenized)
         # Stop words give context to the query
         #simplified = [token for token in tokenized if not token in self.stopwords]
         
@@ -127,37 +152,124 @@ class AnalystAI:
         for s in self.inMemoryData:
             print(s['NLPMapping'])
 
+
+
+
         print(' ')
 
+    def previewQuestion(self, question:str):
+        tokenized = nltk.word_tokenize(question.lower())
+        stemmedPOS = nltk.pos_tag(tokenized)
+        test = nltk.pos_tag(str.split(question,' '))
+        print(stemmedPOS)
+        print(test)
 
 
 
+
+
+
+
+    def connectQuestionContextLibrary(self, csvPath:str = 'FlaskApp/sample/POSClassification.csv'):
+        
+        if not os.path.exists(csvPath):
+            
+            f = open(csvPath, 'w+')
+            f.write('Index,OpeningPOS,QuestionLength,NounCount,Output')
+            f.close()
+
+        self.contextLibPath = csvPath
+      
+    def trainQuestionContext(self, question:str, context:str):
+        tags = nltk.pos_tag(str.split(question,' '))
+        contextLib = pd.read_csv(self.contextLibPath, index_col=['Index'])
+
+        try:
+            
+            index = len(contextLib)
+            openingPOS = tags[0][1]
+            questionLength = len(tags)
+
+            nounCount = 0
+            for tag in tags:
+                if 'NN' in tag[1]:
+                    nounCount += 1
+
+            contextLib.loc[index] = pd.Series(dict(OpeningPOS=openingPOS, QuestionLength = questionLength, NounCount = nounCount, Output = context))
+        
+        except:
+            print('Something went wrong')
+        
+        contextLib.to_csv(self.contextLibPath, index='Index')
+
+    def classifyQuestionContext(self, question:str):
+        encodedSet = self.runContextEncoder()
+        
+        clf = svm.SVC()
+        
+        train = [list(se) for ind, se in encodedSet.loc[:, encodedSet.columns != 'Output'].iterrows()]
+        print(train)
+        clf.fit(train, encodedSet['Output'])
+
+        tags = nltk.pos_tag(str.split(question,' '))
+        pos_OHE = self.encodeOpeningPOS([tags[0][1]])[0]
+        sentenceLen = len(tags)
+        nounCount = 0
+        for tag in tags:
+            if 'NN' in tag[1]:
+                nounCount += 1
+
+        print([pos_OHE,sentenceLen,nounCount])
+
+        print(clf.predict([[pos_OHE,sentenceLen,nounCount]]))
+        for key in CONTEXTRESULT_ENCODER.keys():
+            if CONTEXTRESULT_ENCODER[key] == clf.predict([[pos_OHE,sentenceLen,nounCount]]):
+                print('Classified: ' + key)
+                break        
+        pass
+
+    def runContextEncoder(self):
+        
+        contextLib = pd.read_csv(self.contextLibPath, index_col=['Index'])
+        encodedContextLib = copy(contextLib)
+        
+        openingPOS_OHE = self.encodeOpeningPOS(contextLib['OpeningPOS'])
+        
+        target = []
+        for context in contextLib['Output']:
+            target.append(CONTEXTRESULT_ENCODER[context])
+
+        encodedContextLib['OpeningPOS'] = openingPOS_OHE
+        encodedContextLib['Output'] = target
+
+        return encodedContextLib
+
+    def encodeOpeningPOS(self, openPOS: list):
+        openingPOS_OHE = []
+        for posTag in openPOS:
+            if '$' in posTag:
+                posTag = posTag[0:-1]
+
+            POS_ENCODER[posTag]
+            openingPOS_OHE.append(POS_ENCODER[posTag])
+        
+        return openingPOS_OHE
 
 analyst = AnalystAI()
 analyst.importData_excel('FlaskApp/sample/Sample - Superstore.xls', 'Orders')
 analyst.trainModel()
-analyst.processQuestion('What is my most profitable product')
-analyst.processQuestion('What is my top profitable product')
-'''
-analyst.processQuestion("What is the total sales in the last 3 months?")
-analyst.processQuestion("Show monthly sales over the last 3 months.")
-analyst.processQuestion("What is the sales trend?")
-analyst.processQuestion("What is the monthly sales trend?")
-analyst.processQuestion("Rank the top 5 product categories by average sales.")
-analyst.processQuestion("Rank the top 5 average sales by product categories.")
-analyst.processQuestion("Rank the top 5 product category by average sales in the last 3 years.")
-analyst.processQuestion("Rank the top 5 product category by average sales in the last 3 years with sales more than 2000.")
-analyst.processQuestion("Show the top 10 performing products in the last 10 months.")
-analyst.processQuestion("What are the top 10 performing products in the last 10 months")
-analyst.processQuestion("What is the average monthly sales by product category?")
-analyst.processQuestion("What does sales look like in the next 3 months")
-analyst.processQuestion("What does sales look like in the next three months")
-analyst.processQuestion("Who are my top clients in year 2022?")
-analyst.processQuestion("Show average monthly sales for product equals diamonds")
-analyst.processQuestion("Show average monthly sales for product is diamonds")
-analyst.processQuestion("Show average monthly sales for diamond products")
-analyst.processQuestion("Show monthly sales where product is car")
-analyst.processQuestion("Show monthly sales where product is car or bicycle")
-analyst.processQuestion("Show monthly sales where product is car and color is blue")
-analyst.processQuestion("Show monthly sales of the diamond product")
-'''
+
+analyst.connectQuestionContextLibrary()
+
+analyst.trainQuestionContext("Show me my expenses?", "General")
+analyst.trainQuestionContext("What is my most profitable product?", "Specific")
+analyst.trainQuestionContext("What is the total sales in the last 3 months?", "Specific")
+analyst.trainQuestionContext("How is business going?", "General")
+analyst.trainQuestionContext("What is the monthly sales trend?", "Specific")
+analyst.trainQuestionContext("Rank the top 5 average sales by product categories.", "Specific")
+
+analyst.classifyQuestionContext('How is business doing?')
+
+
+encoder = skprocess.LabelEncoder()
+print(analyst.model.predict([encoder.fit_transform(pd.Series({'i_OverridingRequest':'Rank','i_Discovery':'sum','i_DiscoveryField':'Profit','i_DiscoveryFieldType':'float','i_BreadkdownField':'Product Name','i_BreakdownFieldType':'string'}))]))
